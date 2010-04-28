7fcd3f6da1402772fb093c168019ee88b082e2c3
petr999 <petr999@e3989258-e93d-1bda-f1cd-fef770077d64>
2010-04-28 12:27:57 +0000

    more on POD
    git-svn-id: https://nossa.googlecode.com/svn@35 e3989258-e93d-1bda-f1cd-fef770077d64

--- lib/Net/OpenID/Server/Standalone/Config.pm
+++ lib/Net/OpenID/Server/Standalone/Config.pm
@@ -6,11 +6,33 @@ use warnings;
 our $config;
 
 =pod
-  my $config = {
+
+=head1 NAME
+
+  Net::OpenID::Server::Standalone::Config - configuration package for Nossa
+
+=head1 SYNOPSIS
+
+used internally by Net::OpenID::Server::Standalone. The only sub is:
+
+=head2 get( @args )
+
+follows your $config as a hash of hashes consequentially.
+
+=head2 $config
+
+used to store all your configuration like this:
+     
+  our $config = {
+    #users stuff
     users => {
+      # nickname is a key
       'the_your_nickname' => {
+        # hashFunction from password, MD5 by default
         pass        => 'md5_base64 of your password',
+        # OpenID URL
         url         => 'http://your.openid.url/',
+        # function to recognise sites that you trust to be asking about your OpenID, you'd be asked about it otherwise.
         trust_root  => sub{
                             shift =~ m/(blogger\.com|cpan\.org|ccmixter\.org|stickr\.com|mychores\.co\.uk\/openid|qdos\.com
                                         |demand-openid\.rpxnow\.com|livejournal\.com|sourceforge\.net)\/?$/x;
@@ -22,18 +44,26 @@ our $config;
         },
       },
     },
-  # Where to redirect in case of wrong login/pass, wrong OpenID url, or
-  # failed filter for RP:
+    # Where to redirect in case of wrong login/pass or the wrong OpenID url
     setupUrl => '/setup',
+    # your id script URL
     idSvrUrl => '/id',
+    # your OpenID server key
     serverSecret => 'some_random_sequence_put_your_own',
+    # whether to redirect a user to the SSL URL of id
     requireSsl=> 0,
+    # arguments for L<CGI::Session>
     session  =>  {
       dsn  =>"driver:DB_File;serializer:FreezeThaw",
       name  => 'nossa_cookie',
       expire  => '+1h',
     },
   };
+
+=head1 ETCETERA
+
+is at L<Net::Server::OpenID::Standalone>.
+
 =cut
 
 ###  No user-serviceable part below this line ###
